f7199fc96a0de338e7907e2ce8511a27787c195c
john.connor <john.connor@inversa.tech>
2018-11-15 08:19:49 -0600

    Union de todos los models

---
+++ HW/Models/Cliente.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// Agregar los namespaces necesarios
+using System.Data;
+using System.Data.SqlClient;
+
+namespace GenisysATM.Models
+{
+    class Cliente
+    {
+        // Propiedades
+        public int id { get; set; }
+        public string nombres { get; set; }
+        public string apellidos { get; set; }
+        public string identidad { get; set; }
+        public string direccion { get; set; }
+        public string telefono { get; set; }
+        public string celular { get; set; }
+
+        // Métodos
+        /// <summary>
+        /// Obtiene un cliente desde la tabla ATM.Cliente
+        /// </summary>
+        /// <param name="identidad">La identidad del cliente (13 caracteres)</param>
+        /// <returns>Un objeto de tipo Cliente.</returns>
+        public static Cliente ObtenerCliente(string identidad)
+        {
+            Conexion conexion = new Conexion(@"(local)", "GenisysATM_V2");
+            string sql;
+            Cliente resultado = new Cliente();
+
+            // Query SQL
+            sql = @"SELECT *
+                    FROM ATM.Cliente
+                    WHERE identidad = @identidad";
+
+            SqlCommand cmd = conexion.EjecutarComando(sql);
+            SqlDataReader rdr;
+
+            try
+            {
+                using (cmd)
+                {
+                    cmd.Parameters.Add("@identidad", SqlDbType.Char, 13).Value = identidad;
+
+                    rdr = cmd.ExecuteReader();
+                }
+
+                while (rdr.Read())
+                {
+                    resultado.id = rdr.GetInt16(0);
+                    resultado.nombres = rdr.GetString(1);
+                    resultado.apellidos = rdr.GetString(2);
+                    resultado.identidad = rdr.GetString(3);
+                    resultado.direccion = rdr.GetString(4);
+                    resultado.telefono = rdr.GetString(5);
+                    resultado.celular = rdr.GetString(6);
+
+                    // Remover espacios
+                }
+
+                return resultado;
+            }
+            catch (SqlException ex)
+            {
+                return resultado;
+            }
+            finally
+            {
+                conexion.CerrarConexion();
+            }
+
+        }
+
+        /// <summary>
+        /// Método para la inserción de datos
+        /// </summary>
+        public static bool InsertarCliente(Cliente nuevoCliente)
+        {
+            Conexion conexion = new Conexion(@"(local)", "GenisysATM_V2");
+            SqlCommand cmd = conexion.EjecutarComando("sp_insertarCliente");
+
+            // Establecer el comando como un Stored Procedure
+            cmd.CommandType = CommandType.StoredProcedure;
+
+
+            // Parámetros del Stored Procedure
+            cmd.Parameters.Add(new SqlParameter("@nombre", SqlDbType.NVarChar, 100));
+            cmd.Parameters["@nombre"].Value = nuevoCliente.nombres;
+            cmd.Parameters.Add(new SqlParameter("@apellido", SqlDbType.NVarChar, 100));
+            cmd.Parameters["@apellido"].Value = nuevoCliente.apellidos;
+            cmd.Parameters.Add(new SqlParameter("@identidad", SqlDbType.Char, 13));
+            cmd.Parameters["@identidad"].Value = nuevoCliente.identidad;
+            cmd.Parameters.Add(new SqlParameter("@direccion", SqlDbType.NVarChar, 2000));
+            cmd.Parameters["@direccion"].Value = nuevoCliente.direccion;
+            cmd.Parameters.Add(new SqlParameter("@telefono", SqlDbType.Char, 9));
+            cmd.Parameters["@telefono"].Value = nuevoCliente.telefono;
+            cmd.Parameters.Add(new SqlParameter("@celular", SqlDbType.Char, 9));
+            cmd.Parameters["@celular"].Value = nuevoCliente.celular;
+
+            // intentamos ejecutar el procedimiento
+            try
+            {
+                conexion.EstablecerConexion();
+
+                cmd.ExecuteNonQuery();
+
+                return true;
+            }
+            catch (SqlException ex)
+            {
+               // MessageBox.Show(ex.StackTrace);
+
+                return false;
+            }
+            finally
+            {
+                conexion.CerrarConexion();
+            }
+
+        }
+
+    }
+}
